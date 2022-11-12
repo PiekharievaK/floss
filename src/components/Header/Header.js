432e9bf70db44673af62f8b22b27abcf16a67cf4
PiekharievaK <pechareva@gmail.com>
2022-11-12 04:46:21 +0200

    cnbkn

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,11 @@ import s from "../Header/Header.module.scss"
 export const Header =()=>{
     return<div>
          <ul>
-            <li><a href="/">Homepage</a> </li>
-            <li><a href="/ColorsPage">ColorsPage</a> </li>
-            <li><a href="/UserPage">UserPage</a> </li>
-            <li><a href="/JournalPage">Joural</a> </li>
-            <li><a href="/RegisterPage">Register</a> </li>
+            <li className={s.li}><a href="/">Homepage</a> </li>
+            <li className={s.li}><a href="/ColorsPage">ColorsPage</a> </li>
+            <li className={s.li}><a href="/UserPage">UserPage</a> </li>
+            <li className={s.li}><a href="/JournalPage">Joural</a> </li>
+            <li className={s.li}><a href="/RegisterPage">Register</a> </li>
         </ul>
     </div>
 }
