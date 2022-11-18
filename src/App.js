0f34162eabd180733e56542aff8b98eb998855be
PiekharievaK <pechareva@gmail.com>
2022-11-18 17:43:54 +0200

    путь

--- src/App.js
+++ src/App.js
@@ -1,62 +1,55 @@
-import { Header } from './components/Header/Header';
-import {Outlet, Route, Routes } from 'react-router-dom';
-import './App.css';
-import { Homepage } from './Pages/Homepage/HomePage';
-import { UserPage } from './Pages/UserPage/Userpage';
-import { JournalPage } from './Pages/JournalPage/JournalPage';
-import { RegisterPage } from './Pages/RegisterPage/RegisterPage';
-import {ColorsPage} from "./Pages/ColorsPage/ColorsPage"
+import { Header } from "./components/Header/Header";
+import { Outlet, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Homepage } from "./Pages/Homepage/HomePage";
+import { UserPage } from "./Pages/UserPage/Userpage";
+import { JournalPage } from "./Pages/JournalPage/JournalPage";
+import { RegisterPage } from "./Pages/RegisterPage/RegisterPage";
+import { ColorsPage } from "./Pages/ColorsPage/ColorsPage";
 // import { PrivateRoute, PublicRoute } from './components/Routers';
-
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-      <Route  path="/"  element={<Outlet/>}>
-                <Route index  element={<Homepage />}/>
-                <Route
-                  path="/ColorsPage"
-                  element={
-                    // <PublicRoute>
-                    <ColorsPage />
-                    /* </PublicRoute> */
-                  }
-                  />
-                 <Route
-                  path="/Registerpage"
-                  element={
-                    // <PublicRoute>
-                    <RegisterPage />
-                    /* </PublicRoute> */
-                  }
-                />
-                <Route
-                  path={'/UserPage'}
-                  element={
-                    // <PrivateRoute>
-                    <UserPage />
-                    // </PrivateRoute>
-                  }
-                  />
-                <Route
-                  path={'/JournalPage'}
-                  element={
-                    // <PrivateRoute>
-                      <JournalPage
-                        />
-                    // </PrivateRoute>
-                  }
-                  />
-                 <Route
-                  path="*"
-                  element={
-                    <Homepage />
-                  }
-                  />
-                 </Route>
-            </Routes>
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<Homepage />} />
+          <Route
+            path="ColorsPage"
+            element={
+              // <PublicRoute>
+              <ColorsPage />
+              /* </PublicRoute> */
+            }
+          />
+          <Route
+            path="Registerpage"
+            element={
+              // <PublicRoute>
+              <RegisterPage />
+              /* </PublicRoute> */
+            }
+          />
+          <Route
+            path={"UserPage"}
+            element={
+              // <PrivateRoute>
+              <UserPage />
+              // </PrivateRoute>
+            }
+          />
+          <Route
+            path={"JournalPage"}
+            element={
+              // <PrivateRoute>
+              <JournalPage />
+              // </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<Homepage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
