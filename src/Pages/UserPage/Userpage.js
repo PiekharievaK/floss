d13b4e62ca3f8de792892d70b7ed5d560c6e1372
PiekharievaK <pechareva@gmail.com>
2022-12-17 08:46:31 +0200

    правит путь ссылки к журналу со страницы профиля

--- src/Pages/UserPage/Userpage.js
+++ src/Pages/UserPage/Userpage.js
@@ -6,7 +6,7 @@ export  const UserPage =()=>{
     return <Container><h1>UserPage</h1>
     <ul className={s.list}>
         <li className={s.item}>Profile <span>(developing)</span></li>
-        <li className={s.item}><Link to="./JoutnalPage" className={s.link}>My floss </Link></li>
+        <li className={s.item}><Link to="/JournalPage" className={s.link}>My floss </Link></li>
         <li className={s.item}>My schemas <span>(developing)</span></li>
         <li className={s.item}><a  className={s.link} href="https://doitms.com/csm/" target="blank">Create schema by image online </a></li>
         <li className={s.item}><a  className={s.link} href="https://moresxem.com/category/sxemi-pdf/" target="blank">Find schemas </a></li> 
